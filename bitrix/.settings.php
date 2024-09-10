886df05c34e2810851960e2f3096254260f6e07a
HulaHoopoe <hoopoe45.89@gmail.com>
2024-09-10 13:44:31 +0500

    first commit - create empty bitrix project

---
+++ bitrix/.settings.php
@@ -0,0 +1,64 @@
+<?php
+return array (
+  'utf_mode' => 
+  array (
+    'value' => true,
+    'readonly' => true,
+  ),
+  'cache_flags' => 
+  array (
+    'value' => 
+    array (
+      'config_options' => 3600.0,
+      'site_domain' => 3600.0,
+    ),
+    'readonly' => false,
+  ),
+  'cookies' => 
+  array (
+    'value' => 
+    array (
+      'secure' => false,
+      'http_only' => true,
+    ),
+    'readonly' => false,
+  ),
+  'exception_handling' => 
+  array (
+    'value' => 
+    array (
+      'debug' => false,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
+      'ignore_silence' => false,
+      'assertion_throws_exception' => true,
+      'assertion_error_type' => 256,
+      'log' => NULL,
+    ),
+    'readonly' => false,
+  ),
+  'connections' => 
+  array (
+    'value' => 
+    array (
+      'default' => 
+      array (
+        'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+        'host' => 'localhost',
+        'database' => 'sosbrodovu_bxdb',
+        'login' => 'sosbrodovu_bxdb',
+        'password' => 'MQ!34ZKSv1cH65CH',
+        'options' => 2.0,
+      ),
+    ),
+    'readonly' => true,
+  ),
+  'crypto' => 
+  array (
+    'value' => 
+    array (
+      'crypto_key' => 'ad3ac66920965feefffb9ba4df2b8fa5',
+    ),
+    'readonly' => true,
+  ),
+);
